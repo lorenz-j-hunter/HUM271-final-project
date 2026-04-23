c89482b1a26b133325af044d3f40ade9865a7625
LorenzJHunter <lhunter1@iwu.edu>
2026-04-23 18:32:28 -0500

    progress on README specs

--- app.py
+++ app.py
@@ -2,36 +2,57 @@ from flask import Flask, render_template, request
 import requests # pyright: ignore[reportMissingModuleSource]
 
 # All URL endpoints used.
-url = "https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed?actor=mrsbettybowers.bsky.social"
-
-getActors = "https://public.api.bsky.app/xrpc/app.bsky.actor.searchActors"
+# We use 'actors' to get a query of said actor's feed. 
+actors_endpoint = "https://public.api.bsky.app/xrpc/app.bsky.actor.searchActors"
 
 # All endpoint parameters used.
-params = {
-  "limit" : 1
-}
 
-getActorsParams = {
+actors_params: dict[str, str | int] = {
   "q" : "", # an empty string returns `general users`. 
   "limit" : 5 
 }
 
 # The responses created with these endpoints and their parameters.
-response = requests.get(
-  url, params
+actors: requests.Response = requests.get(
+  actors_endpoint, actors_params
 )
 
-actorsResponse = requests.get(
-  getActors, getActorsParams
+# Now that we have a list of actors, we will search their feed.
+feeds_endpoint = "https://public.api.bsky.app/xrpc/app.bsky.feeds/getActorFeeds"
+feeds_params: dict[str, str | int] = {
+  "actor" : "",
+  "limit" : 5
+}
+
+actors_feeds: requests.Response = requests.get(
+  feeds_endpoint, feeds_params 
 )
 
 # Open the app
 app = Flask(__name__)
 
+print(actors.json())
+
 @app.route('/', methods=['GET','POST'])
 def main():
-  """Open the main route for the app."""
-  posts = response.json().get('feed', None)[0].get('post', 'There was no post.').get('record', 'There was no record.')
-  text = posts.get('text')
-  print(text)
-  return render_template('main.html', response=response.json())
+  """Open the main route for the app.
+
+  In Bluesky, the text from any post in the JSON is arranged like so:
+  feed > post > record > text
+
+  If you want to traverse by feed/fetched stuff, then change the `[0]` here to whatever number you choose,
+  provided it is less than `limit` in your params.
+
+  If you want stuff other than the text, like the author, you can play with `get('post')`. 
+  """
+  text: list[str] = []
+  for post in range(actors_params.get('limit')): # type: ignore
+    # We gather all of the columns for the CSV at this point.
+    # Those four columns were (1) actor, (2) gender, (2) feed, (3) Perceived opinion.
+    # We create four variables now, each of which represents a column 
+    actor = actors_feeds.json().get('feed', None)[post].get('creator').get('displayName')
+    pronouns = actors_feeds.json().get('feed', None)[post].get('creator').get('pronouns')
+    feed = actors_feeds.json().get('feed', None)[post].get('creator').get('pronouns')
+    opinion = actors_feeds.json().get('feed', None)[post].get('creator').get('associated').get('chat')
+    text.append(opinion.get('text'))
+  return render_template('main.html', text=text)
