85c0b2bfd18116f27f0fea890999740d0ebe43c7
LorenzJHunter <lhunter1@iwu.edu>
2026-05-01 12:14:56 -0500

    wip

--- utility/classes.py
+++ utility/classes.py
@@ -5,12 +5,36 @@
 # append an item. This makes it possible to retrieve data from the 3D database.
 #
 # An item has the did, data, and platform all in one. 
+
+empty: dict = {
+    'data' : '',
+    'did' : '',
+    'platform' : '',
+    'type' : ''
+}
 class item:
     data: str = ''
     did: str = ''
     platform: str = ''
+    type: str = ''
+    def __init__(self, args: dict):
+        self.data=args['data']
+        self.did=args['did']
+        self.platform=args['platform']
+        self.type=args['type']
+    def __str__(self):
+        return self.data + '_' + self.did + '_' + self.platform + '_' + self.type
 
-def ret() -> item:
-    """Return an empty item."""
-    ret: item = item()
-    return ret
+
+def to_item(string: str) -> item:
+    """Return an item which represents the string repr that was
+    sent int.
+    
+    Accept a string repr of an item. Return an item"""
+    r: item = item(empty)
+    datum: list[str] = string.split('_')
+    r.data = datum[0]
+    r.did = datum[1]
+    r.platform = datum[2]
+    r.type = datum[3]
+    return r
