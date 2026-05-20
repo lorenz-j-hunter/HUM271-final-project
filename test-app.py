a873f254a4f083ff876f4039399bdb1b1c5dff8e
LorenzJHunter <lhunter1@iwu.edu>
2026-05-19 19:13:51 -0500

    unit testing update

--- test-app.py
+++ test-app.py
@@ -13,17 +13,52 @@ class Tests(unittest.TestCase):
 		os.close(self.db_fd)
 		os.unlink(webscraping.app.config['DATABASE'])
 
+
+	"""Bluesky tests."""
+
+
 	def test_enter_bsky(self):
+		"""Enter the site and submit."""
 		bluesky = self.app.post('/bluesky', data=dict(
 			bluesky_length='5'
 		))
 		assert bluesky.status_code == 200
 
+
+	def test_bsky_csv(self):
+		"""Enter the site, get a csv, and check to see if it has the right format."""
+		# Ensure that there is a csv to analyze. 
+		self.app.get('/bluesky', data=dict(
+			bluesky_length='5'
+		))
+		with open(os.path.relpath('../csvfiles/bluesky.csv')) as file:
+			text = file.readline()
+			# Assert that cells were rendered correctly. 
+			for item in text.split(','):
+				assert '#' not in item[0], item[len(item)]
+			# Assert that there are the correct number of columns inserted.
+#			assert len(text.split(',')) == 5 
+
+	def test_required_bluesky_0(self):
+		"""Ensure that it is required to enter `bluesky_length`."""
+		bluesky = self.app.get('/bluesky', data=dict())
+		assert bluesky.status_code != 200		
+
+	"""X tests."""
+
 	def test_enter_x(self):
+		"""Ensure we can enter the page and submit the data."""
 		x = self.app.post('/x', data=dict(
 			x_length='5'
 		))
 		assert x.status_code == 200
+
+	def test_required_x_0(self):
+		"""Ensure that it is required to enter `x_length`."""
+		x = self.app.get('/x', data=dict())
+		assert x.status_code != 200	
+
+	"""Pornhub tests."""
 
 	def test_enter_pornhub_0(self):
 		"""Enter pornhub without entering tags or pornstars."""
@@ -31,6 +66,29 @@ class Tests(unittest.TestCase):
 			pornhub_length='5'
 		))
 		assert pornhub.status_code == 200
+
+	def test_enter_pornhub_1(self):
+		"""Enter pornhub by entering tags, not pornstars."""
+		pornhub = self.app.post('/pornhub', data=dict(
+			pornhub_length='5',
+			tags=['oiled']
+		))
+		assert pornhub.status_code == 200
+
+	def test_enter_pornhub_2(self):
+		"""Enter pornhub by entering tags and pornstars."""
+		pornhub = self.app.post('/pornhub', data=dict(
+			pornhub_length='5',
+			tags=['cum'],
+			pornstars=['Evie Christian']
+		))
+		assert pornhub.status_code == 200
+
+	def test_required_pornhub_0(self):
+		"""Entering `pornhub_length` should be required to submit the form."""
+		pornhub = self.app.get('/pornhub', data=dict())
+		assert pornhub.status_code != 200
+
 
 if __name__ == "__main__":
 	unittest.main()
