5244468ae0b0eb83840403e7eaf125f848381d9b
LorenzJHunter <lhunter1@iwu.edu>
2026-05-24 16:37:24 -0500

    update remote

--- static/activate_b.js
+++ static/activate_b.js
@@ -1,14 +1,24 @@
 /*We define a function in which a link initially is hidden, but becomes visible
 once the user completes certain actions.*/
+/*We define a function for which the stream link is initially hidden, but becomes visible
+once the user enters their stream data.*/
 document.addEventListener('DOMContentLoaded', () => {
   //By default, the link is invisible. 
   if (!localStorage.getItem('visibility_b')) {
     localStorage.setItem('visibility_b', 'content-visibility: hidden;');
   }
+  if (!localStorage.getItem('visibility_b_stream')) {
+    localStorage.setItem('visibility_b_stream', 'content-visibility: hidden;');
+  }
   document.querySelector('#csv').style = localStorage.getItem('visibility_b')
+  document.querySelector('#stream-csv').style = localStorage.getItem('visibility_b_stream')
 });
 // When the user clicks the button, the link becomes visible and remains so after page reloads.
 function activate() {
   document.querySelector('#csv').style = 'content-visibility: visible;';
   localStorage.setItem('visibility_b', 'content-visibility: visible;');
+}
+function activate_stream() {
+   document.querySelector('#stream-csv').style = 'content-visibility: visible;';
+  localStorage.setItem('visibility_b_stream', 'content-visibility: visible;'); 
 }
