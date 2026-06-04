53b0b9c5048a488aaaab9164dc51ce75783a5bf0
LorenzJHunter <lhunter1@iwu.edu>
2026-06-03 20:30:50 -0500

    fix parallax bug

--- static/parallax_bsky.js
+++ static/parallax_bsky.js
@@ -1,43 +1,46 @@
 startpoint = 50; 
 endpoint = 100;
 
-const layers = [
-  {
-    el: document.querySelector(".parallax-bsky"),
-    start: startpoint,
-    end: endpoint,
-    speed: 80  
-  }
-];
-
-['scroll', 'DOMContentLoaded'].forEach(event => {
-  window.addEventListener(event, () => {
-    const y = window.scrollY;
-
-    for (const layer of layers) {
-      const { el, start, end, speed } = layer;
-      console.log(`el=${el}`);
-
-      if (y < start) {
-        // FIXED MODE (before range)
-        el.style.backgroundAttachment = "fixed";
-        el.style.backgroundPositionY = `-${start}px`;
-        continue;
-      }
-
-      if (y > end) {
-        // FIXED MODE (after range)
-        el.style.backgroundAttachment = "fixed";
-        el.style.backgroundPositionY = `-${end}px`;
-        continue;
-      }
-
-      // TRANSFORM MODE (between start and end)
-      el.style.backgroundAttachment = "scroll";
-      const progress = (y - start) / (end - start); // 0 → 1
-      const offset = progress * speed;
-
-      el.style.backgroundPositionY = `-${offset}px`;
+document.addEventListener('DOMContentLoaded', () => {
+  const layers = [
+    {
+      el: document.querySelector(".parallax-bsky"),
+      start: startpoint,
+      end: endpoint,
+      speed: 80  
     }
-  })
+  ];
+
+  ['scroll', 'DOMContentLoaded'].forEach(event => {
+    window.addEventListener(event, () => {
+      const y = window.scrollY;
+
+      for (const layer of layers) {
+        const { el, start, end, speed } = layer;
+        console.log(`el=${el}`);
+
+        if (y < start) {
+          // FIXED MODE (before range)
+          el.style.backgroundAttachment = "fixed";
+          el.style.backgroundPositionY = `-${start}px`;
+          continue;
+        }
+
+        if (y > end) {
+          // FIXED MODE (after range)
+          el.style.backgroundAttachment = "fixed";
+          el.style.backgroundPositionY = `-${end}px`;
+          continue;
+        }
+
+        // TRANSFORM MODE (between start and end)
+        el.style.backgroundAttachment = "scroll";
+        const progress = (y - start) / (end - start); // 0 → 1
+        const offset = progress * speed;
+
+        el.style.backgroundPositionY = `-${offset}px`;
+      }
+    })
+  });
 });
+
