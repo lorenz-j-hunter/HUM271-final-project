ebd4938a1cb085eb9596da7deded8f0947ed30e4
LorenzJHunter <lhunter1@iwu.edu>
2026-06-10 16:36:12 -0500

    csv bug

--- logic/insertion.py
+++ logic/insertion.py
@@ -63,11 +63,11 @@ async def bsky(db_path, params={'bluesky_length': 10, 'limit': 100, 'querystring
       continue 
     insertion_list: list[str] = []
     for i in range(posts_limit):
-      print(f'Are we here yet? (line 66)')
+      print(f'Are we here yet? ({i}) (line 66)')
       # insert a post uri.
       try:
         insertion_list.append(post_response.json().get('posts')[i].get('uri'))
-      except:
+      except IndexError:
         break
     all_posts[identifier] = insertion_list
     # Now here we gather all follow data of the user in question.
@@ -85,18 +85,17 @@ async def bsky(db_path, params={'bluesky_length': 10, 'limit': 100, 'querystring
       # insert the user ID of the follow.
       insertion_list: list[str] = []
       for i in range(follows_limit):
-        print(f'Are we here yet? (line 88)')
+        print(f'Are we here yet? ({i}) (line 88)')
         try:
           insertion_list.append(follows_response.json().get('follows')[i].get('did'))
-        except:
+        except IndexError:
           break
       all_follows[identifier] = insertion_list 
     else: # In this case, we still have to fill all_follows with something.
       all_follows[identifier] = [] 
-  # Finally, we add these columns to a database.
+  # Finally, we add these columns to a database, actor by actor.
   # We first insert to the first dimension here, then move onto the second dimension.
   for actor in range(bluesky_length): 
-    # the age in months must be computed from the real response data.
     age_months: int = get_age(actors['actors'][actor].get('createdAt', 'None'))
     db.execute('INSERT INTO first_dim_for_bluesky (name, did, age_months, pronouns) VALUES (?, ?, ?, ?)',
                 [actors['actors'][actor].get('displayName', 'None'),
@@ -107,43 +106,44 @@ async def bsky(db_path, params={'bluesky_length': 10, 'limit': 100, 'querystring
     # Now we add to the second dimension for follows and posts. 
     # First we insert into both columns for follows. Then, we update the rows
     # that have been filled with posts.
-    for _ in range(follows_limit):
-      f_insertion_list: list[str] = all_follows[identifiers[actor]]
-      for e in range(len(f_insertion_list)):
-        print('\tinsert into second_dim (follows, line 114)')
-        db.execute('INSERT INTO second_dim_for_bluesky (follows, posts, item_id) VALUES (?, ?, ?)',
-                  [f_insertion_list[e],
-                    'None',
-                    str(actor)])
+    # follows:
+    f_insertion_list: list[str] = all_follows[identifiers[actor]]
+    for e in range(len(f_insertion_list)):
+      print('\tinsert into second_dim (follows, line 112)')
+      db.execute('INSERT INTO second_dim_for_bluesky (follows, posts, item_id) VALUES (?, ?, ?)',
+                [f_insertion_list[e],
+                  'None',
+                  str(actor)])
+      db.commit()
+    # posts:
+    f_insertion_list_len: int = len(all_follows[identifiers[actor]])
+    p_insertion_list: list[str] = all_posts[identifiers[actor]]
+    p_insertion_list_len: int = len(p_insertion_list)
+    # If there are more follows than posts, there is a case for that. 
+    # likewise for posts > follows and posts == follows.
+    if p_insertion_list_len > f_insertion_list_len:
+      for e in range(f_insertion_list_len):
+        print('\tupdate second_dim (follows, line 128) (f < p)')
+        db.execute('UPDATE second_dim_for_bluesky SET posts = (?) WHERE item_id == (?)',
+                    [p_insertion_list[e], e])
         db.commit()
-    for _ in range(posts_limit):
-      f_insertion_list_len: int = len(all_follows[identifiers[actor]])
-      p_insertion_list: list[str] = all_posts[identifiers[actor]]
-      p_insertion_list_len: int = len(p_insertion_list)
-      # If there are more follows than posts, there is a case for that. 
-      # likewise for posts > follows.
-      if p_insertion_list_len > f_insertion_list_len:
-        for e in range(f_insertion_list_len):
-          print('\tupdate second_dim (follows, line 128) (f < p)')
-          db.execute('UPDATE second_dim_for_bluesky SET posts = (?) WHERE item_id == (?)',
-                      [p_insertion_list[e], e])
-          db.commit()
-        for e in range(len(p_insertion_list) - f_insertion_list_len):
-          print('\tinsert into second_dim (follows, line 132)')
-          db.execute('INSERT INTO second_dim_for_bluesky (follows, posts, item_id) VALUES (?, ?, ?)',
-                      ['None',
-                        p_insertion_list[e],
-                        str(actor)])
-          db.commit()
-      elif f_insertion_list_len > p_insertion_list_len | f_insertion_list_len == p_insertion_list_len:
-        for e in range(p_insertion_list_len):
-          print('\tupdate second_dim (follows, line 141) (f > p | f == p)')
-          db.execute('UPDATE second_dim_for_bluesky SET posts = (?) WHERE item_id == (?)',
-                      [p_insertion_list[e], e])
-          db.commit()
-  # Create the csv.
+      for e in range(len(p_insertion_list) - f_insertion_list_len):
+        print('\tinsert into second_dim (follows, line 132)')
+        db.execute('INSERT INTO second_dim_for_bluesky (follows, posts, item_id) VALUES (?, ?, ?)',
+                    ['None',
+                      p_insertion_list[e],
+                      str(actor)])
+        db.commit()
+    elif f_insertion_list_len > p_insertion_list_len | f_insertion_list_len == p_insertion_list_len:
+      for e in range(p_insertion_list_len):
+        print('\tupdate second_dim (follows, line 141) (f > p | f == p)')
+        db.execute('UPDATE second_dim_for_bluesky SET posts = (?) WHERE item_id == (?)',
+                    [p_insertion_list[e], e])
+        db.commit()
+  # Create the csv. This downloads it to their computer.
   get_bluesky_csv(db)
-  # Finally, we let the state know this operation is complete.
+  # Finally, we let the state know this operation is complete
+  # and stop the thread (loop.stop()).
   print('Completed request, stopping worker')
   db.execute('UPDATE worker_done SET value = (?) WHERE value = (?)', ['true', 'false']) 
   db.commit()
