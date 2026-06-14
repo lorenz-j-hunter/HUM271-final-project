b8bf8a65dc05ea18066bbf0cce862a1d4e222d11
LorenzJHunter <lhunter1@iwu.edu>
2026-06-13 21:42:27 -0500

    make database larger

--- logic/websocket.py
+++ logic/websocket.py
@@ -19,149 +19,131 @@ def backfill(db_path):
 threading.Thread(target=loop_runner, daemon=True).start()
 
 
-async def jetstream_batches(stream, type, batch_size=50000):
-  batch = []
-  async for event in stream:
-    if type == 'post':
-      pass
-    elif type == 'follow':
-      pass
-    parsed = parse(event)
-    if parsed is not None:
-      batch.append(parsed)
+def get_event_type(parsed: dict[str,str]) -> str:
+  if parsed['status'] == 'success':
+    if parsed['type'] == 'app.bsky.graph.follow':
+      return  'follow'
+    elif parsed['type'] == 'app.bsky.feed.post':
+      return 'post'
+  return 'neither'
 
+
+async def jetstream_batches(stream, type, batch_size=10, max_events=100_000_000, querystring: str | None='a'):
+  batch = []
+  print('in batch generator')
+  async for event in stream:
+    try:
+      # yield batch early if disconnect.
+      if event['disconnect'] == True:
+        batch.insert(0, ['disconnect', event.get('cursor')])
+        break
+    except KeyError:
+      pass
+    parsed = await parse(event, params={'querystring': querystring})
+    event_type = get_event_type(parsed)
+    # filter by event typ
+    if type == event_type == 'post' and parsed['status'] == 'success':
+      line: list = [
+        parsed['author_id'],
+        parsed['text'], 
+        parsed['created_at']
+      ]
+      batch.append(line)
+    elif type == event_type == 'follow' and parsed['status'] == 'success':
+      line: list = [
+        parsed['follower'],
+        parsed['followee'], 
+        parsed['created_at'],
+        'false', 
+        parsed['rkey']
+      ]
+      batch.append(line)
+    
+    # if a batch's size is not batch_size at time of yield, there is a case for that here
     if len(batch) == batch_size:
       yield batch
       batch = []
+
+    if max_events == 0:
+      break
+
 
   if batch:
     yield batch
 
 
-async def worker(db_path, type):
+async def jetstream_worker(db_path, params={'max_events': '5', 'type': 'post'}):
   """Write the rows from the batch generator"""
-  conn = sqlite3.connect(db_path, check_same_thread=False)
-  cur = conn.cursor()
-  cur.execute("BEGIN TRANSACTION;")
-  if type == 'post':
-    async for batch in jetstream_batches(jetstream_stream(), type=type):
-        cur.executemany(
-            'INSERT INTO posts (author_id, text, created_at) VALUES (?, ?, ?)',
-            batch
-        )
-  elif type == 'follow':
-    async for batch in jetstream_batches(jetstream_stream(), type=type):
-        cur.executemany(
-            '''INSERT OR IGNORE INTO follows (follower, followee, created_at, blocked, rkey)
-                      VALUES (?, ?, ?, ?, ?)''',
-            batch
-        )
-        cur.executemany('''INSERT OR IGNORE INTO profiles
-                      (did, display_name, avatar_cid, banner_cid, website, pronouns, created_at, rkey)
-                      VALUES (?,?,?,?,?,?,?,?)''', batch)
-  elif type == 'block':
-    async for batch in jetstream_batches(jetstream_stream(), type=type):
-        cur.executemany(
-            'UPDATE follows SET blocked = (?) WHERE follower = (?) AND followee = (?)',
-            batch
-        )
-  cur.execute("COMMIT;")
-
-
-async def jetstream_stream(cursor=None):
-  """Open the stream."""
-  url = None
-  if cursor:
-    url = f'wss://jetstream2.us-east.bsky.network/subscribe?cursor={cursor}'
-  else:
-    url = "wss://jetstream2.us-east.bsky.network/subscribe"
-
-  async with websockets.connect(url) as ws:
-    while True:
-      msg = await ws.recv()
-      try:
-        data = json.loads(msg)
-      except json.JSONDecodeError:
-        continue  # skip malformed messages
-
-      yield data
-
-
-async def jetstream_worker(db_path, params={'max_events': 100, 'event_type': 'post'}):
-  """Get from the stream."""
-  # Here is something that lets you pause until worker is done.
-  count = 0
-  cursor = None
-  # unpack params.
+  # unpack params
   max_events = params['max_events']
   event_type = params['event_type']
   mark_blocks = params['mark_blocks'] if params['mark_blocks'] == 'yes' else None
   querystring = params['querystring'] if params['querystring'] else None
-  # Any event that comes through must match the event type.
-  if event_type == 'post':
-    event_type = 'app.bsky.feed.post'
-  elif event_type == 'follow':
-    event_type = 'app.bsky.graph.follow'
-  # sift through the stream.
-  # If there is a disconnect, reconnect where you left off.
-  while True:
-    isfailure = None
-    async for event in jetstream_stream(cursor):
-      ret: dict = await parse(event, params={'querystring': querystring})
-      isfailure = event.get('cursor')
-      # Only add successful messages.      
-      if ret['status'] == 'success':
-        db = sqlite3.connect(db_path, check_same_thread=False)
-        db.row_factory = sqlite3.Row
-        # Add to the database (posts)
-        if ret['path'].find(event_type) != -1 and 'app.bsky.feed.post' == event_type:
-          db.execute('INSERT INTO posts (author_id, text, created_at) VALUES (?, ?, ?)',
-                  [ret['author_id'], ret['text'], ret['created_at']])
-          count += 1
-        # Add to the database. (follows)
-        elif ret['path'].find(event_type) != -1 and 'app.bsky.graph.follow' == event_type:
-          db = sqlite3.connect(db_path, check_same_thread=False)
-          db.row_factory = sqlite3.Row
-          # a person has followed someone
-          if ret['op'] == 'create':
-            db.execute('''INSERT OR IGNORE INTO follows (follower, followee, created_at, blocked, rkey)
-                      VALUES (?, ?, ?, ?, ?)''',
-                      [ret['follower'], ret['followee'], ret['created_at'], 'false', ret['rkey']])
-            # Create their profiles database for later.
-            db.execute('''INSERT OR IGNORE INTO profiles
-                      (did, display_name, avatar_cid, banner_cid, website, pronouns, created_at, rkey)
-                      VALUES (?,?,?,?,?,?,?,?)''',
-                      [ret['follower'],'','','','','','',ret['rkey']])
-            count += 1
-          # a person has unfollowed. 
-          elif ret['op'] == 'delete':
-            db.execute('DELETE FROM follows WHERE rkey = (?)',
-                            [ret['rkey']])
-        elif ret['path'].find('app.bsky.graph.block') != -1 and mark_blocks:
-          db = sqlite3.connect(db_path, check_same_thread=False)
-          db.row_factory = sqlite3.Row
-          db.execute('UPDATE follows SET blocked = (?) WHERE follower = (?) AND followee = (?)',
-                    ['true', ret['follower'], ret['followee']])
-      # We stop when we have exceeded the desired limit
-      if count >= max_events:
-        print("Reached max events, stopping worker")
-        db = sqlite3.connect(db_path, check_same_thread=False)
-        db.row_factory = sqlite3.Row
-        # Tell the system that the worker is done. 
-        db.execute('UPDATE worker_done SET value = (?) WHERE value == (?)', ['true', 'false'])
-        stream_files.get_jetstream_csv(db_path=db_path, columns={
-          'mark_blocks': mark_blocks
-          })
-        db.commit()
-        db.close()
-        break
-    # If the connection was unexpectedly close, reset where left off.
-    if count < max_events:
-      cursor = isfailure
-    else:
-      break
 
-async def parse(event, params={}):
+  conn = sqlite3.connect(db_path, check_same_thread=False)
+  conn.row_factory = sqlite3.Row
+  conn.execute('BEGIN TRANSACTION;')
+  remaining = max_events
+  cursor = None
+  async for batch in jetstream_batches(jetstream_stream(cursor=cursor), type=event_type, max_events=max_events, querystring=querystring):
+    message = batch[0]
+    if message[0] == 'disconnect':
+      # insert the partially filled batch then reconnect
+      cursor = message[1]
+      rows = batch[1:]
+      break 
+    else:
+      rows = batch
+      cursor = None
+    if event_type == 'post':
+      conn.executemany(
+        'INSERT INTO posts (author_id, text, created_at) VALUES (?, ?, ?)',
+        rows 
+      )
+    else:
+      conn.executemany(
+        '''INSERT OR IGNORE INTO follows (follower, followee, created_at, blocked, rkey)
+                  VALUES (?, ?, ?, ?, ?)''',
+        rows
+      )
+    remaining -= len(rows)
+    if remaining <= 0:
+      break
+  conn.execute('UPDATE worker_done SET value = (?) WHERE value == (?)', ['true', 'false'])
+  conn.execute('COMMIT;') 
+  print('Completed jetstream worker, stopping')
+  stream_files.get_jetstream_csv(db_path=db_path, columns={
+    'mark_blocks': mark_blocks 
+    })
+  conn.close()
+
+
+async def jetstream_stream(cursor=None):
+  url = (
+    "wss://jetstream2.us-east.bsky.network/subscribe"
+    if not cursor else
+    f"wss://jetstream2.us-east.bsky.network/subscribe?cursor={cursor}"
+  )
+  cursor = None
+  try:
+    async with websockets.connect(url) as ws:
+      while True:
+        msg = await ws.recv()
+        try:
+          data = json.loads(msg)
+          cursor = data.get('cursor')
+        except json.JSONDecodeError:
+          continue  # skip malformed messages
+
+        yield data
+  except websockets.exceptions.ConnectionClosed:
+    # stream disconnects
+    yield {'disconnect': True, 'cursor': cursor}
+  except Exception:
+    # timeout, network error, etc
+    yield {'disconnect': True, 'cursor': cursor}
+
+async def parse(event, params={}) -> dict:
   """Selectively add to the events list"""
   ret: dict = {}
   querystring = params['querystring'] if params['querystring'] else None
@@ -180,10 +162,10 @@ async def parse(event, params={}):
     return ret
 
   ret['status'] = 'success'
-  ret['path'] = record.get('$type')
+  ret['type'] = record.get('$type')
 
   # if the desired type is a post.
-  if ret['path'].find('app.bsky.feed.post') != -1:
+  if ret['type'].find('app.bsky.feed.post') != -1:
     ret['author_id'] = event.get('did')
     ret['text'] = 'title was none' if not record.get('text') else record.get('text')
     ret['created_at'] = record.get('createdAt')
@@ -194,14 +176,7 @@ async def parse(event, params={}):
         ret['status'] = 'failure'
         return ret
   # if the desired type is a follow
-  elif ret['path'].find('app.bsky.graph.follow') != -1:
-    ret['followee'] = record.get('subject')
-    ret['follower'] = event.get('did')
-    ret['created_at'] = record.get('createdAt')
-    ret['op'] = commit.get('operation')
-    ret['rkey'] = commit.get('rkey')
-  # if the desired type if a block.
-  elif ret['path'].find('app.bsky.graph.block') != -1:
+  elif ret['type'].find('app.bsky.graph.follow') != -1:
     ret['followee'] = record.get('subject')
     ret['follower'] = event.get('did')
     ret['created_at'] = record.get('createdAt')
